04bac118eab41677e01f9d58152de52b0464c830
Khanh Le <lednhatkhanh@gmail.com>
2026-09-17 11:44:35 +0700

    Import cache namespace from owning module

--- packages/cache/src/next-handler.ts
+++ packages/cache/src/next-handler.ts
@@ -1,6 +1,7 @@
 import { Redis } from "ioredis";
 
-import { createCacheNamespace, createRedisCacheHandler, type RedisCacheHandler } from "./index.ts";
+import { createCacheNamespace } from "./cache-namespace.ts";
+import { createRedisCacheHandler, type RedisCacheHandler } from "./index.ts";
 
 const client = new Redis(process.env["REDIS_URL"] ?? "redis://127.0.0.1:6379", {
   lazyConnect: true,
